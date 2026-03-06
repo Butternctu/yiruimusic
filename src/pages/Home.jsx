49953002516b4ba221eca307204dd3d5aa9daf2a
Nathan_Wu <nathan.cl.wu@fii-na.com>
2026-03-06 10:30:23 -0600

    seo: add FAQPage JSON-LD schema for Google rich results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import SEO from '../components/SEO';
 import Hero from '../components/Hero';
 import About from '../components/About';
@@ -6,10 +7,27 @@ import Academic from '../components/Academic';
 import PerformancePreview from '../components/PerformancePreview';
 import FAQ from '../components/FAQ';
 import Contact from '../components/Contact';
+import { faqs } from '../data/faqs';
+
+const faqSchema = {
+  '@context': 'https://schema.org',
+  '@type': 'FAQPage',
+  mainEntity: faqs.map(faq => ({
+    '@type': 'Question',
+    name: faq.question,
+    acceptedAnswer: {
+      '@type': 'Answer',
+      text: faq.answer,
+    },
+  })),
+};
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <script type="application/ld+json">{JSON.stringify(faqSchema)}</script>
+      </Helmet>
       <SEO title="Dr. Yirui Li, Harpist & Educator | Houston, Texas" url="/" />
       <Hero />
       <About />
